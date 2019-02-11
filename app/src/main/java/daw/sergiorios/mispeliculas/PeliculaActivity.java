c6cad6f08d5bce4b8bbf7b5dda579b20b298f064
Sergio Ríos <43775911+sergio-rios@users.noreply.github.com>
2019-02-11 18:53:17 +0100

    Sustitución de botones guardar|borrar por menú de opciones

--- app/src/main/java/daw/sergiorios/mispeliculas/PeliculaActivity.java
+++ app/src/main/java/daw/sergiorios/mispeliculas/PeliculaActivity.java
@@ -9,6 +9,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -45,7 +48,16 @@ public class PeliculaActivity extends AppCompatActivity {
 
         getDatosIntent();
     }
-    
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.pelicula_opciones, menu);
+
+        return true;
+    }
+
     private void getDatosIntent() {
         if (getIntent().hasExtra("id")) {
             id = getIntent().getStringExtra("id");
@@ -106,22 +118,24 @@ public class PeliculaActivity extends AppCompatActivity {
         Log.d("vista", vista);
     }
 
-    public void manejarDatos(View v) {
-        switch (v.getId()) {
-            case R.id.btnBorrar:
-                Log.d("BORRAR", id);
-                AlertDialog alert = mostrarConfirmacion(titulo);
-                alert.show();
-                break;
 
-            case R.id.btnGuardar:
-                Log.d("ACTUALIZAR", id);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.guardar:
                 getDatosActualizar();
                 managerPelicula.actualizar(id, titulo, director, productora, duracion, anio, valoracion, sinopsis, vista, imagen);
                 Toast.makeText(this, "Pelicula guardada", Toast.LENGTH_SHORT).show();
                 break;
+            case R.id.borrar:
+                AlertDialog alert = mostrarConfirmacion(titulo);
+                alert.show();
+                break;
         }
+
+        return super.onOptionsItemSelected(item);
     }
+
 
     private void getDatosActualizar() {
         titulo = etTitulo.getText().toString();
